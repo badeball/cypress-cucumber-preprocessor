8fcac925d47e93edc90989b7fb39d94b04121d8f
Jonas Amundsen <jonasba@gmail.com>
2026-08-22 19:01:35 +0200

    Update example to account for duckduckgo markup change
    This has apparently changed again.

--- examples/esbuild-ts/cypress/e2e/duckduckgo.ts
+++ examples/esbuild-ts/cypress/e2e/duckduckgo.ts
@@ -5,7 +5,7 @@ When("I visit duckduckgo.com", () => {
 });
 
 Then("I should see a search bar", () => {
-  cy.get("input[type=text]")
+  cy.get("textarea")
     .should("have.attr", "placeholder")
     .and("match", /Search without being tracked|Search privately/);
 });
